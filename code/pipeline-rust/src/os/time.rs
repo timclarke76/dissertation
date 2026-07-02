5eab997c0dcc10def15c92d5ad9bd266f9fcda03
Tim Clarke <timothyclarke@gmail.com7>
2026-07-02 08:48:09 +0100

    Correct cargo doc comment.

--- code/pipeline-rust/src/os/time.rs
+++ code/pipeline-rust/src/os/time.rs
@@ -3,9 +3,9 @@ use anyhow::{Context, Result};
 use nix::time::{ClockId, clock_gettime};
 
 /// Returns the current monotonic time in nanoseconds.
-/// #Returns
-/// A `Result` containing the current monotonic time in nanoseconds, or an error
-/// if the operation fails.
+///
+/// Returns a `Result` containing the current monotonic time in nanoseconds, or
+/// an error if the operation fails.
 pub fn now_nanos() -> Result<u64> {
     let ts = clock_gettime(ClockId::CLOCK_MONOTONIC)
         .context("Failed to get monotonic time")?;
