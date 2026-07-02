2f6e19e8056e969e75f724d7c540a12483d990d6
Tim Clarke <timothyclarke@gmail.com7>
2026-07-02 11:47:46 +0100

    Add C++ ShmBuffer.

---
+++ code/pipeline-cpp/src/os/shm.cpp
@@ -0,0 +1,115 @@
+#include <cstring>
+#include <fcntl.h>
+#include <format>
+#include <iostream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <os/os.h>
+#include <os/time.h>
+
+#include "shm.h"
+
+ShmBuffer::ShmBuffer(const std::string_view& name, const size_t stream_id)
+  : name_(name)
+  , stream_id_(stream_id)
+{
+  header_ = open_shm_file();
+
+  if (header_->magic != Header::MAGIC) {
+    throw std::runtime_error(std::format(
+      "Memory corruption or invalid magic number in shared memory '{}'",
+      name_));
+  }
+
+  if (header_->version != Header::VERSION) {
+    throw std::runtime_error(std::format(
+      "Incompatible version in shared memory '{}': expected {}, got {}",
+      name_,
+      Header::VERSION,
+      header_->version));
+  }
+
+  data_ptr_ = reinterpret_cast<char*>(header_) + sizeof(Header);
+  capacity_frames_ = header_->capacity_frames;
+  frame_size_bytes_ = header_->frame_size_bytes;
+}
+
+ShmBuffer::Frame
+ShmBuffer::next_frame()
+{
+  for (;;) {
+    // The producer increments the sequence number after writing a frame, using
+    // Release ordering to ensure all previous writes have been flushed. The
+    // consumer uses Acquire ordering to ensure it sees the latest writes to the
+    // frame data.
+    const uint64_t seq_num = header_->seq_num.load(std::memory_order_acquire);
+
+    if (seq_num > frame_idx_) {
+      if (seq_num - frame_idx_ > header_->capacity_frames) {
+        // The producer has lapped the consumer, which means that frames have
+        // been overwritten before they could be read. Print a warning to stderr
+        // and jump ahead to the oldest surviving frame. For example, if the
+        // producer is at sequence 35 and the capacity is 30, the oldest
+        // surviving frame is at sequence 6.
+        std::cerr << std::format(
+          "[WARNING] {} producer lapped consumer by {} frames.\n",
+          name_,
+          (seq_num - frame_idx_));
+
+        frame_idx_ = seq_num - capacity_frames_;
+      }
+
+      const auto circular_idx =
+        static_cast<size_t>(frame_idx_ % capacity_frames_);
+      const auto data_offset = circular_idx * frame_size_bytes_;
+      const auto data = this->data_ptr_ + data_offset;
+
+      Frame frame = {
+        stream_id_, seq_num, { 0, current_time_nanos(), 0, 0, 0, 0 }
+      };
+
+      std::memcpy(&frame.timestamps[0], data, sizeof(uint64_t));
+
+      frame_idx_++;
+
+      return frame;
+    }
+
+    spin_loop();
+  }
+}
+
+ShmBuffer::Header*
+ShmBuffer::open_shm_file()
+{
+  const int fd = shm_open(name_.c_str(), O_RDONLY, 0);
+
+  if (fd == -1) {
+    throw std::runtime_error(std::format(
+      "Failed to open shared memory '{}': {}", name_, std::strerror(errno)));
+  }
+
+  struct stat sb;
+
+  if (fstat(fd, &sb) == -1) {
+    close(fd);
+    throw std::runtime_error(
+      std::format("Failed to get metadata for shared memory '{}': {}",
+        name_,
+        std::strerror(errno)));
+  }
+
+  void* ptr = mmap(nullptr, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
+
+  if (ptr == MAP_FAILED) {
+    close(fd);
+    throw std::runtime_error(std::format(
+      "Failed to map shared memory '{}': {}", name_, std::strerror(errno)));
+  }
+
+  close(fd);
+
+  return static_cast<Header*>(ptr);
+}
