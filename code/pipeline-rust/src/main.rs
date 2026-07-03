4650114affc5b5272145547a6acdf0b7206d9553
Tim Clarke <timothyclarke@gmail.com7>
2026-07-03 10:23:24 +0100

    Tidy up Rust main file.

--- code/pipeline-rust/src/main.rs
+++ code/pipeline-rust/src/main.rs
@@ -25,6 +25,45 @@ use thread::{
 #[global_allocator]
 static GLOBAL: TrackingAllocator = TrackingAllocator;
 
+/// Creates a bridge thread and an inference thread for a given shared memory
+/// name, queue capacity, and backpressure policy.
+///
+/// * `stream_name` - The name of the stream to be used for shared memory and
+///   telemetry.
+/// * `stream_id` - The stream ID associated with the bridge thread.
+/// * `queue_capacity` - The maximum number of frames that can be held in the
+///   queue.
+/// * `inference_sender` - A `SyncSender<ShmFrame>` used to send
+///   processed frames to the fusion thread.
+/// * `policy` - The backpressure policy to apply when the queue is full.
+///
+/// Returns a tuple containing the `JoinHandle`s of the spawned bridge and
+/// inference threads.
+fn create_bridge_and_inference_threads(
+    stream_name: &str,
+    stream_id: usize,
+    queue_capacity: usize,
+    inference_sender: SyncSender<ShmFrame>,
+    policy: Policy,
+    inference_time: Duration,
+) -> (JoinHandle<()>, JoinHandle<()>) {
+    let queue = Arc::new(Mutex::new(Queue::<ShmFrame>::new(queue_capacity)));
+
+    let bridge =
+        spawn_bridge_thread(stream_name, stream_id, &queue, policy)
+            .expect("Failed to spawn bridge thread");
+
+    let inference = spawn_inference_thread(
+        stream_name,
+        &queue,
+        inference_sender,
+        inference_time,
+    )
+    .expect("Failed to spawn inference thread");
+
+    (bridge, inference)
+}
+
 fn main() -> Result<()> {
     let args = Args::try_parse()?;
     let settings = Settings::try_new(args)?;
@@ -93,43 +132,4 @@ fn main() -> Result<()> {
     }
 
     Ok(())
-}
-
-/// Creates a bridge thread and an inference thread for a given shared memory
-/// name, queue capacity, and backpressure policy.
-/// #Args
-/// * `stream_name` - The name of the stream to be used for shared memory and
-///   telemetry.
-/// * `stream_id` - The stream ID associated with the bridge thread.
-/// * `queue_capacity` - The maximum number of frames that can be held in the
-///   queue.
-/// * `inference_sender` - A `SyncSender<ShmFrame>` used to send
-///   processed frames to the fusion thread.
-/// * `policy` - The backpressure policy to apply when the queue is full.
-/// #Returns
-/// A tuple containing the `JoinHandle`s of the spawned bridge and inference
-/// threads.
-fn create_bridge_and_inference_threads(
-    stream_name: &str,
-    stream_id: usize,
-    queue_capacity: usize,
-    inference_sender: SyncSender<ShmFrame>,
-    policy: Policy,
-    inference_time: Duration,
-) -> (JoinHandle<()>, JoinHandle<()>) {
-    let queue = Arc::new(Mutex::new(Queue::<ShmFrame>::new(queue_capacity)));
-
-    let bridge_thread =
-        spawn_bridge_thread(stream_name, stream_id, &queue, policy)
-            .expect("Failed to spawn bridge thread");
-
-    let inference_thread = spawn_inference_thread(
-        stream_name,
-        &queue,
-        inference_sender,
-        inference_time,
-    )
-    .expect("Failed to spawn inference thread");
-
-    (bridge_thread, inference_thread)
 }
