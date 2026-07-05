f92289ff91ccc1556883440aea259fe836be0f1f
Tim Clarke <timothyclarke@gmail.com7>
2026-07-05 17:05:53 +0100

    Add Python shared memory buffer.

--- code/pipeline-py/pipeline.py
+++ code/pipeline-py/pipeline.py
@@ -3,6 +3,7 @@ import datetime as dt
 from typing import Final
 
 from config import Args, Policy, QueueConfig, Settings
+from os_ import ShmBuffer
 
 
 @dataclass(frozen=True)
@@ -19,19 +20,19 @@ def main():
     CONFIGS: Final[list[StreamConfig]] = [
         StreamConfig(
             queue=SETTINGS.rgb_queue,
-            stream_id=0,
+            stream_id=ShmBuffer.RGB_STREAM_ID,
             policy=SETTINGS.rgb_policy,
             duration=dt.timedelta(milliseconds=33),
         ),
         StreamConfig(
             queue=SETTINGS.accelerometer_queue,
-            stream_id=1,
+            stream_id=ShmBuffer.ACCEL_STREAM_ID,
             policy=SETTINGS.accelerometer_policy,
             duration=dt.timedelta(microseconds=500),
         ),
         StreamConfig(
             queue=SETTINGS.gyroscope_queue,
-            stream_id=2,
+            stream_id=ShmBuffer.GYRO_STREAM_ID,
             policy=SETTINGS.gyroscope_policy,
             duration=dt.timedelta(microseconds=400),
         ),
@@ -39,6 +40,21 @@ def main():
 
     CHANNEL_SIZE: Final[int] = sum(cfg.queue.capacity_frames for cfg in CONFIGS)
     MIN_FPS: Final[int] = min(cfg.queue.fps for cfg in CONFIGS)
+    BUFFERS: Final[list[ShmBuffer]] = [
+        ShmBuffer(cfg.queue.name, cfg.stream_id) for cfg in CONFIGS
+    ]
+
+    shm_buffer = BUFFERS[0]
+    while True:
+        frame = shm_buffer.next_frame()
+        print(
+            f"Stream ID: {frame.stream_id}, "
+            f"Sequence Number: {frame.seq_num}, "
+            f"Timestamps: {frame.timestamps}"
+        )
+        if frame.seq_num == ShmBuffer.POISON_PILL:
+            print("Received poison pill. Exiting.")
+            break
 
 
 if __name__ == "__main__":
